d2a0295e7546e7129761573d6e9b950d9eebefb4
Birger Schacht <birger.schacht@oeaw.ac.at>
2023-10-17 13:26:21 +0200

    fix placeimport

--- apis_ontology/management/commands/importplaces.py
+++ apis_ontology/management/commands/importplaces.py
@@ -22,14 +22,18 @@ class Command(BaseCommand):
             if res := re.search(pattern, p.references):
                 print(p)
                 uri = f"https://www.wikidata.org/wiki/Special:EntityData/{res.group(1)}.rdf?flavor=simple"
-                model, data = rdf.get_modelname_and_dict_from_uri(uri, configfile)
-                if not p.longitude:
-                    p.longitude = data['longitude']
-                else:
-                    print(f"Longitude already set for {p}")
-                if not p.latitude:
-                    p.latitude = data['latitude']
-                else:
-                    print(f"Latitude already set for {p}")
-                p.save()
+                try:
+                    model, data = rdf.get_modelname_and_dict_from_uri(uri, configfile)
+                    if not p.longitude:
+                        p.longitude = data['longitude']
+                    else:
+                        print(f"Longitude already set for {p}")
+                    if not p.latitude:
+                        p.latitude = data['latitude']
+                    else:
+                        print(f"Latitude already set for {p}")
+                    p.save()
+                except Exception:
+                    print(f"not found {p}")
+                    pass
                 time.sleep(1)
