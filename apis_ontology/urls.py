8ee2c343d4d15a4bfab37db812ca84c8c13dc142
Birger Schacht <birger.schacht@oeaw.ac.at>
2023-11-23 11:59:03 +0100

    feat: add bibsonomy reference api endpoint

--- apis_ontology/urls.py
+++ apis_ontology/urls.py
@@ -1,7 +1,12 @@
 from apis_acdhch_default_settings.urls import urlpatterns
-from django.urls import path
+from django.urls import path, include
 from .views import CustomReferenceDetailView, TempTripleAutocomplete, TempEntityClassAutocomplete, CustomReferenceDeleteView
+from .api import ReferenceViewSet
 from django.contrib.auth.decorators import login_required
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'references', ReferenceViewSet)
 
 customurlpatterns = [
     path("bibsonomy/references/<int:pk>", login_required(CustomReferenceDetailView.as_view()), name='referencedetail'),
@@ -9,4 +14,4 @@ customurlpatterns = [
     path('bibsonomy/temptriple-autocomplete/', TempTripleAutocomplete.as_view(), name="temptriple-autocomplete",),
     path('bibsonomy/references/<int:pk>/delete', login_required(CustomReferenceDeleteView.as_view()), name='referencedelete'),
 ]
-urlpatterns = customurlpatterns + urlpatterns
+urlpatterns = customurlpatterns + urlpatterns + [ path('custom/api/', include(router.urls)) ]
